282f399d664d6b5100fb548e8af8074d138963ae
Daeshaun Willis <ddwillis59@gmail.com>
2023-01-17 22:37:17 -0500

    Added switch statements so buttons are interactive!

--- index.js
+++ index.js
@@ -3,11 +3,39 @@
 for (let i = 0; i<= document.querySelectorAll(".drum").length - 1;i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        
-        this.style.color = "white";
-
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
+        let buttonLetter = this.textContent;
+        switch(buttonLetter){
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            
+            default: console.log(buttonLetter)
+        }
     });
 }
 
